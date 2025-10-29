68d07d90a6ff8e22164147efca6ae10133ef804f
Narottam Sharma <narottamphodegaa@gmail.com>
2025-10-29 23:01:07 +0530

    set collection

--- Demo/src/four/ListDemo.java
+++ Demo/src/four/ListDemo.java
@@ -1,7 +1,15 @@
 package four;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+
+class Car{
+    String brand;
+    Car(String brand){
+        this.brand=brand;
+    }
+}
 
 public class ListDemo {
     static void main(String[] args) {
@@ -14,5 +22,19 @@ public class ListDemo {
         for(Integer user: users){
             IO.println(user);
         }
+        IO.println(users.get(2));
+
+//        List of objects
+        Car car1 = new Car("Toyota");
+        Car car2 = new Car("Mercedes");
+        Car car3 = new Car("Maruti");
+        List<Car> carList = new ArrayList();
+        carList.add(car1);
+        carList.add(car3);
+        carList.add(car2);
+        for(Car car: carList){
+            IO.println(car.brand);
+        }
+
     }
 }
