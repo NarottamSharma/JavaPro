2e0f02780850fb0483d9aa2c8c1da14f55ac90cb
Narottam Sharma <narottamphodegaa@gmail.com>
2025-10-30 10:43:41 +0530

    map and set  collection

--- Demo/src/four/MapDemo.java
+++ Demo/src/four/MapDemo.java
@@ -1,4 +1,18 @@
 package four;
 
-public class Map {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    static void main(String[] args) {
+        Map<Integer,String> userMap = new HashMap<>();
+        userMap.put(1,"Narottam");
+        userMap.put(2,"Gaurav");
+        userMap.put(3,"Akhil");
+        IO.println(userMap);
+        IO.println(userMap.get(2));
+        for(Map.Entry<Integer,String> entry:userMap.entrySet()){
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
 }
--- Demo/src/four/SetDemo.java
+++ Demo/src/four/SetDemo.java
@@ -1,5 +1,21 @@
 package four;
 
-public class SetDemo {
+import java.util.HashSet;
+import java.util.Set;
 
+public class SetDemo {
+    static void main(String[] args) {
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        roles.add("user");
+        roles.add("user");
+        for(String role:roles){
+            IO.println(role);
+        }
+        roles.remove("admin");
+        for(String role:roles){
+            IO.println(role);
+        }
+
+    }
 }
